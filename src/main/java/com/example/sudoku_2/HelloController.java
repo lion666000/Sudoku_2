6c1e6b65d6602a84ed812c38f1befdd705cb8138
hlajak06 <hlajak06@gasos-ro.cz>
2026-03-19 10:45:47 +0100

    f

--- src/main/java/com/example/sudoku_2/HelloController.java
+++ src/main/java/com/example/sudoku_2/HelloController.java
@@ -2,16 +2,35 @@ package com.example.sudoku_2;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 
 public class HelloController {
-
-
     @FXML
     private GridPane gridPane;
+
+    @FXML
+    protected void wrongBase(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Zadání");
+        alert.setHeaderText(null); // Removes the header space
+        alert.setContentText("Chyba v zadání");
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    protected void wrongSolution(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Řešení");
+        alert.setHeaderText(null); // Removes the header space
+        alert.setContentText("Chyba v řešení");
+
+        alert.showAndWait();
+    }
 
     protected void generateNumbers(){
         ArrayList<String> numbers =  new ArrayList<>();
@@ -42,9 +61,33 @@ public class HelloController {
     }
 
     @FXML
+    protected void zadani(){
+        int[][] sudokuGrid = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+
+        for (int j = 0; j < 9; j++){
+            for(int k = 0; k < 9; k++){
+                String n = String.valueOf(sudokuGrid[j][k]);
+                gridPane.add(new TextField(n), j, k);
+            }
+        }
+    }
+
+    @FXML
     protected boolean kontrolujPole(ArrayList<Integer> numbers){
 
         ArrayList<Integer> numberOfInstances = new ArrayList<>();
+
+        boolean ano = false;
 
         for (int i = 1; i <= 9; i++){
             numberOfInstances.add(0);
@@ -55,24 +98,27 @@ public class HelloController {
         for (int i = 0; i < numbers.size(); i++){
             int number = numbers.get(i);
 
-            if (numberOfInstances.get(number-1) != null){
+            if (number == 0){
+                ano = false;
+            }
+            else if (numberOfInstances.get(number-1) != null){
                 int numberTwink = numberOfInstances.get(number-1) + 1;
 
                 numberOfInstances.set(number - 1, numberTwink);
 
-            }
-            else {
+            } else if (number < 1 || number > 9) {
+                ano =  false;
+            } else{
                 numberOfInstances.set(number - 1, 1);
             }
         }
 
         System.out.println(numberOfInstances);
 
-        boolean ano = false;
+
 
         for (int i = 0; i < numberOfInstances.size(); i++){
             if (numberOfInstances.get(i) != 1){
-                System.out.println("NE");
                 ano = false;
                 break;
             }
@@ -80,66 +126,98 @@ public class HelloController {
                 ano = true;
             }
         }
+
         if (ano){
             System.out.println("ANO");
+        }
+        else {
+            System.out.println("NE");
         }
 
         return ano;
     }
 
     @FXML
-    protected void rowCheck(){
-        ArrayList<Integer> numbers = new ArrayList<>();
-
-
-        int column = 0;
-        //int row = 0;
-
-
-
-        for  (Node node : gridPane.getChildren()){
-            if (node instanceof TextField) {
-                TextField tf = (TextField) node;
-                numbers.add(Integer.parseInt(tf.getText()));
-
-                //System.out.println(tf.getText());
-                column++;
-            }
-
-
-
-
-            if (column == 9){
-                //row++;
-                column = 0;
-
-                //System.out.println(numbers);
-
-                kontrolujPole(numbers);
-
-                numbers.clear();
-            }
-        }
-
-
-    }
-
-    @FXML
-    protected void columnCheck(){
+    protected boolean rowCheck(){
         ArrayList<Integer> numbers = new ArrayList<>();
 
 
         int column = 0;
         int row = 0;
 
+        int num = 0;
 
+        boolean correct = false;
+
+        for (int i = 0; i < 9; i++){
+            for  (Node node : gridPane.getChildren()) {
+                if (node instanceof TextField) {
+                    if (gridPane.getColumnIndex(node) == column && gridPane.getRowIndex(node) == row) {
+                        TextField tf = (TextField) node;
+                        if (!tf.getText().equals("") && (Integer.parseInt(tf.getText()) <= 9 && Integer.parseInt(tf.getText()) >= 1)) {
+                            num = Integer.parseInt(tf.getText());
+                        } else {
+                            num = 0;
+                        }
+
+                        numbers.add(num);
+
+                        //System.out.println(tf.getText());
+                        column++;
+                    }
+
+                }
+
+
+                if (column == 9) {
+                    row++;
+                    column = 0;
+
+                    //System.out.println(numbers);
+
+                    System.out.println("\nRow " + row + "\n");
+
+                    correct = kontrolujPole(numbers);
+
+
+                    numbers.clear();
+                }
+
+                if (!correct) {
+                    break;
+                }
+            }
+        }
+
+
+        return correct;
+    }
+
+    @FXML
+    protected boolean columnCheck(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+
+        int column = 0;
+        int row = 0;
+
+        int num = 0;
+
+        boolean correct = false;
 
         for (int i = 0; i < 9; i++){
             for  (Node node : gridPane.getChildren()){
                 if (node instanceof TextField) {
                     if (gridPane.getColumnIndex(node) == column && gridPane.getRowIndex(node) == row){
                         TextField tf = (TextField) node;
-                        numbers.add(Integer.parseInt(tf.getText()));
+                        if (!tf.getText().equals("") && (Integer.parseInt(tf.getText()) <= 9 && Integer.parseInt(tf.getText()) >= 1)){
+                            num = Integer.parseInt(tf.getText());
+                        }
+                        else {
+                            num = 0;
+                        }
+
+                        numbers.add(num);
 
                         //System.out.println(tf.getText());
                         row++;
@@ -155,16 +233,27 @@ public class HelloController {
 
                     System.out.println("\nColumn " +  column + "\n");
 
-                    kontrolujPole(numbers);
+                    correct = kontrolujPole(numbers);
 
                     numbers.clear();
                 }
+
+                if (!correct) {
+                    break;
+                }
             }
         }
+        return correct;
     }
 
     @FXML
-    protected void boxCheck(){
+    protected boolean boxCheck(){
+        int box = 0;
+
+        int num = 0;
+
+        boolean correct = false;
+
         // Loop through each of the 9 boxes
         for (int boxRow = 0; boxRow < 9; boxRow += 3) {
             for (int boxCol = 0; boxCol < 9; boxCol += 3) {
@@ -185,71 +274,84 @@ public class HelloController {
                             if (nodeRow == null) nodeRow = 0;
 
                             if (node instanceof TextField && nodeRow == r && nodeCol == c) {
-                                String text = ((TextField) node).getText();
-                                if (!text.isEmpty()) {
-                                    numbers.add(Integer.parseInt(text));
+                                TextField tf = (TextField) node;
+                                if (!tf.getText().equals("") && (Integer.parseInt(tf.getText()) <= 9 && Integer.parseInt(tf.getText()) >= 1)){
+                                    num = Integer.parseInt(tf.getText());
                                 }
+                                else {
+                                    num = 0;
+                                }
+
+                                numbers.add(num);
                             }
                         }
                     }
                 }
                 // Check the box after filling the array for this specific 3x3
-                kontrolujPole(numbers);
+
+                box++;
+
+                System.out.println("\nBox " +  box + "\n");
+
+                correct = kontrolujPole(numbers);
+
+                if (!correct) {
+                    break;
+                }
             }
         }
 
-        /*
-        ArrayList<Integer> numbers = new ArrayList<>();
-
-
-        int column = 0;
-        int row = 0;
-
-        int boxColumn = 0;
-        int boxRow = 0;
-
-
-
-        for (int i = 0; i < 9; i++){
-            for  (Node node : gridPane.getChildren()){
-                if (node instanceof TextField) {
-                    if (gridPane.getColumnIndex(node) == boxColumn + column && gridPane.getRowIndex(node) == boxRow + row){
-                        TextField tf = (TextField) node;
-                        numbers.add(Integer.parseInt(tf.getText()));
-
-                        //System.out.println(tf.getText());
-                        column++;
-                    }
-                }
-
-                if (column == boxColumn + 3){
-                    if (row != boxRow + 2){
-                        column = 0;
-                        row++;
-                    }
-                    else{
-                        kontrolujPole(numbers);
-                        boxColumn += 3;
-                        row = boxRow;
-                        numbers.clear();
-                    }
-
-                    if (boxColumn == 9){
-                        boxColumn = 0;
-                        boxRow += 3;
-                    }
-                }
-
-
-            }
-        }
-
-         */
+        return  correct;
     }
 
     @FXML
+    protected void checkSudokuSolution(){
+
+        if (rowCheck() && columnCheck() && boxCheck()){
+
+        }
+        else  {
+            wrongSolution();
+        }
+    }
+
+    @FXML
+    protected void checkSudokuBase(){
+
+        if (rowCheck() && columnCheck() && boxCheck()){
+
+        }
+        else  {
+            wrongSolution();
+        }
+    }
+
+    @FXML
+    protected void locker(){
+        for  (Node node : gridPane.getChildren()){
+            if (node instanceof TextField) {
+                node.setDisable(true);
+            }
+        }
+    }
+
+    @FXML
+    protected void deleter(){
+        for  (Node node : gridPane.getChildren()){
+            if (node instanceof TextField) {
+                TextField tf = (TextField) node;
+                tf.setDisable(false);
+                tf.setText(null);
+            }
+        }
+    }
+
+
+    @FXML
     protected void initialize(){
-        generateNumbers();
+        gridPane.getChildren().removeAll();
+        //generateNumbers();
+        zadani();
         rowCheck();
         System.out.println("--------------------------------------------------------------------------------------------");
         columnCheck();
